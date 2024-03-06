c9204dec0f5edf4407d1e40f875c5529e789d945
IgorAdelino <igoradelino@gmail.com>
2024-03-05 23:12:02 -0300

    First commit

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const dbConnect = require('./config/dbConnect')
+const dotenv = require('dotenv').config()
+const authRouter = require('./routes/auth-route')
+const { notFound, errorHandler } = require('./middlewares/errorHandler')
+const app = express()
+const PORT = process.env.PORT || 4000
+
+dbConnect()
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api/user', authRouter)
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(PORT, ()=> {
+  console.log(`Server is running at PORT ${PORT}`)
+})
+
